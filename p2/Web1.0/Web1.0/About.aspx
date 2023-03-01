dbcd28f0ba210772dc482b19a148c5dc63b498bb
jmorenog1986 <jhonmorenogarcia@gmail.com>
2023-02-28 21:04:35 -0500

    Se crea eventos en la web

--- p2/Web1.0/Web1.0/About.aspx
+++ p2/Web1.0/Web1.0/About.aspx
@@ -1,7 +1,15 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Web1._0.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
     <h3>Your application description page.</h3>
     <p>Use this area to provide additional information.</p>
+    <!--Formulario locochon-->
+
+     <asp:TextBox ID="txtDocumento" runat="server"></asp:TextBox>
+     <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+     <asp:TextBox ID="txtApellido" runat="server"></asp:TextBox>
+     <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+     <asp:Button ID="BtnGuardar" runat="server" Text="Guardar" OnClick="BtnGuardar_Click"></asp:Button>
+     
+
 </asp:Content>
--- p2/Web1.0/Web1.0/About.aspx.cs
+++ p2/Web1.0/Web1.0/About.aspx.cs
@@ -4,14 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Web1._0
 {
     public partial class About : Page
     {
+
+        SqlConnection conexion;
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        public void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            String sql = "INSERT INTO personas VALUES('" + txtDocumento.Text + "','" + txtNombre.Text + "','" + txtApellido.Text + "','" + txtEmail.Text + "')";
+            Acciones(sql);
+
+        }
+
+
+
+        public void Conectar()
+        {
+            conexion = new SqlConnection("Data Source=192.168.1.7;Initial Catalog=db01;Persist Security Info=True;User ID=SQLServer01;Password=123456");
+            conexion.Open();
+
+        }
+
+        public void CerrarConexion()
+        {
+            conexion.Close();
+        }
+
+
+
+        public void BtnActualizar_Click(object sender, EventArgs e)
+        {
+
+            String sql = "UPDATE personas SET nombre='" + txtNombre.Text + "', apellido='" + txtApellido.Text + "', email='" + txtEmail.Text + "' WHERE documento='" + txtDocumento.Text + "'";
+            Acciones(sql);
+
+
+        }
+        public void Acciones(String sql)
+        {
+            Conectar();
+            SqlCommand comando = new SqlCommand(sql, conexion);
+            int b = comando.ExecuteNonQuery();
+            if (b == 1)
+            {
+                
+            }
+
+            CerrarConexion();
+        }
+
+        public void BtnEliminar_Click(object sender, EventArgs e)
+        {
+
+            String sql = "DELETE FROM personas  WHERE documento='" + txtDocumento.Text + "'";
+            Acciones(sql);
+        }
+
     }
 }
